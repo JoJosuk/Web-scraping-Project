f076d3e2a8599d046c3fe518f4ada6af3b36ea3b
notS0LD13R <joelsebin@gmail.com>
2023-03-16 16:59:32 +0530

    Created ItemObj class to structure a representation for items. Created Amazon class to obtain a list of ItemObjs for a certain item from amazon site

---
+++ Extensions/Amazon.py
@@ -0,0 +1,68 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+from ItemObj import ItemObj
+
+class Amazon:
+    def __init__(self,itemName:str) -> None:
+        #Predefine params for souping tags
+        self.datacomptype='s-search-result'
+        self.span_price='a-price-whole'
+        
+        self.itemName=itemName
+        self.itemPage:BeautifulSoup=BeautifulSoup(
+            self.getPage(self.getSearchLink()).content,'html.parser'
+            )
+        self.itemList=self.getList()
+
+    def __str__(self):
+        s="\n".join([str(x) for x in self.itemList])
+        return s
+
+    def getSearchLink(self):
+        return f'https://www.amazon.in/s?k={self.itemName}'
+    
+    def getPage(self,link:str):
+        return requests.get(link)
+    
+    def getList(self):
+        item_cards=self.itemPage.find_all(
+            'div',
+            attrs={'data-component-type':self.datacomptype}
+            )
+        
+        #First div is not an item card
+        item_cards=item_cards[1:]
+
+        itemList=[self.cardtoItemObj(item) for item in item_cards]
+        return itemList
+
+    def cardtoItemObj(self,card:bs4.element.Tag):
+        try:
+            name=card.h2.text
+            
+            price=card.find('span',class_=self.span_price).text
+            #Remove commas and convert to float
+            price=float(price.replace(',',''))
+
+            img=card.img['src']
+
+            hlink=card.a['href']
+            return ItemObj(
+                name,
+                price,
+                img,
+                hlink
+            )
+        except Exception as err:
+            print(err)
+            return ItemObj(error=err)
+
+
+    
+
+
+
+
+if __name__=='__main__':
+    print(Amazon('car'))
---
+++ Extensions/ItemObj.py
@@ -0,0 +1,39 @@
+#Class to objectify each item
+class ItemObj:
+    """
+    A DataObject representing an item
+
+    Properties:
+        name(str): Name of the item
+        price(float): Price of the item
+        img(str): Link to the image of the item
+        hlink(str): Hyperlink to the items page
+        error(bool): Exception object if error occurs
+    """
+    def __init__(
+            self,
+            name:str=None,
+            price:float=None,
+            img:str=None,
+            hlink:str=None,
+            error:Exception=None,
+            ) -> None:
+        self.name=name
+        self.price=price
+        self.img=img
+        self.hlink=hlink
+        self.error=error
+    
+    def __str__(self):
+        if self.error:
+            empty=""
+            return f'{empty:-<60}\n{self.error}' 
+        
+        empty=""
+        s=f'{empty:-<60}\n\
+        Name:{self.name[:50]}\n\
+        Price:{self.price}\n\
+        Image:{self.img[:50]}\n\
+        HyperLink:{self.hlink[:50]}\n'
+
+        return s
