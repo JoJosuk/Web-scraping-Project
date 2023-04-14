0b6861a7f00bf611547c0bd1fdf451f0805145ed
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-04-15 00:32:39 +0530

    added item give to templates with request

--- radon/core/views.py
+++ radon/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+def itemgive(request):
+    itemname=request.GET['item']
+    return render(request, 'core/itemgive.html',{'itemname':itemname})
--- radon/radon/urls.py
+++ radon/radon/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 
 from core.views import index
+from core.views import itemgive
 urlpatterns = [
     path('', index,name='index'),
     path('admin/', admin.site.urls),
+    path('itemgive',itemgive,name='itemgive'),
 ]
