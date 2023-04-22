7d1d77bba2e7c6c379ff53752073043232354a09
Ashmin Jayson <ashminjayson10@gmail.com>
2023-04-22 18:56:58 +0530

    Added image build and execute description

--- radon/mypythonlib/scrape.py
+++ radon/mypythonlib/scrape.py
@@ -122,7 +122,12 @@ def getdata(itemname):
         # itemPage=requests.get(pageurl,headers={'User-Agent': 'Mozilla/5.0'}).content
         for enum in range(1,2):
             
-            driver = webdriver.Chrome()
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument('--disable-dev-shm-usage')     
+            driver = webdriver.Chrome(options=chrome_options)
             driver.get(pageurl2+str(enum))
             itemPage=driver.page_source
             idk = BeautifulSoup(itemPage,'html.parser')
